cdb03f155a8d4ef0faddb8645e846747a84924c2
Vatsal <sonivatsal111@gmail.com>
2019-04-04 10:45:45 +0530

    Inferencing functionality added

--- project/request_manager.py
+++ project/request_manager.py
@@ -1,11 +1,12 @@
 from flask import Flask,request, render_template
 from werkzeug import secure_filename
-import os,json,schedule
+import os,json
 from threading import Thread
 from time import sleep
 import time
 import datetime
 import requests
+import numpy
 
 UPLOAD_FOLDER = './'
 ALLOWED_EXTENSIONS = set(['txt', 'json', 'png', 'jpg', 'jpeg', 'gif', 'zip'])
@@ -27,6 +28,54 @@ def show_post(post_id):
 def index():
     return render_template('index.html')
 
+@app.route('/inference')
+def inference():
+    return render_template('inference.html')
+
+@app.route('/inferenceService', methods=['GET', 'POST'])
+def inferenceService():
+	model_name = request.form['model_name']
+	model_file = request.files['model_file']
+	action_file = request.files['action_file']
+	print(model_name)
+	if model_file.filename == '':
+		return "Test Data file not found"
+
+	if action_file.filename == '':
+		return "Action file not found"
+
+	if model_file and allowed_file(model_file.filename):
+		filename = secure_filename(model_file.filename)
+		model_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		folder = filename
+
+	if action_file and allowed_file(action_file.filename):
+		filename = secure_filename(action_file.filename)
+		action_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+	# Fetch details from db 
+
+	deployIp = "10.42.0.157"
+	deployPort = 8501
+	url = "http://"+deployIp+":"+str(deployPort)+"/v1/models/"+model_name+":predict"
+
+	file1 = open(model_file.filename,"r+")  
+	inputData = file1.read()
+	inputData = list(inputData.split(", ")) 
+
+	formatData = []
+	for j in inputData:
+		formatData.append(float(j))
+
+	dict_data = {"signature_name":"predict_images","instances":[{"images":formatData}]}
+	data1 = json.dumps(dict_data)
+	response=requests.post(url,data=data1)
+	# print(response)
+	data = response.json()
+
+	# Threading for notification service
+
+	return str(numpy.argmax(data['predictions'][0]))
 
 def allowed_file(filename):
     return '.' in filename and \
